cb9cef4b28c0ce597b9161fbd29a5cb4fcaaafe8
Matteo Fiorina <fiorematteo2002@gmail.com>
2023-12-18 14:45:59 +0100

    day 18

---
+++ 2023/src/day18.rs
@@ -0,0 +1,143 @@
+use std::collections::HashSet;
+
+#[aoc(day18, part1)]
+fn part1(input: &str) -> usize {
+    let mut map = HashSet::new();
+    let mut y = 0;
+    let mut x = 0;
+    map.insert((y, x));
+    for line in input.lines() {
+        let mut s = line.split_whitespace();
+        let dir: Direction = s.next().unwrap().into();
+        let steps: i64 = s.next().unwrap().parse().unwrap();
+        for _ in 0..steps {
+            (y, x) = dir.apply(y, x);
+            map.insert((y, x));
+        }
+    }
+    assert!(!map.contains(&(1, 1)));
+    flood_fill(&mut map, 1, 1);
+    map.len()
+}
+
+#[aoc(day18, part2)]
+fn part2(input: &str) -> i64 {
+    let mut y = 0;
+    let mut x = 0;
+    let mut vertex = vec![(y, x)];
+    let mut area = 2;
+    for line in input.lines() {
+        let mut s = line.split_whitespace().skip(2);
+        let s = s.next().unwrap();
+        let s = &s[2..s.len() - 1];
+        let (steps, dir) = s.split_at(5);
+        let steps = i64::from_str_radix(steps, 16).unwrap();
+        let dir: Direction = dir.into();
+        (y, x) = dir.apply_s(y, x, steps);
+        area += steps;
+        vertex.push((y, x));
+    }
+
+    // sholace formula
+    for v in vertex.windows(2) {
+        let (y0, x0) = v[0];
+        let (y1, x1) = v[1];
+        area += (y0 + y1) * (x0 - x1);
+    }
+    area / 2
+}
+
+fn flood_fill(map: &mut HashSet<(i64, i64)>, y: i64, x: i64) {
+    for (dy, dx) in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
+        let ny = y + dy;
+        let nx = x + dx;
+        if !map.contains(&(ny, nx)) {
+            map.insert((ny, nx));
+            flood_fill(map, ny, nx);
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+enum Direction {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+impl From<&str> for Direction {
+    fn from(value: &str) -> Self {
+        match value {
+            "R" => Direction::Right,
+            "D" => Direction::Down,
+            "L" => Direction::Left,
+            "U" => Direction::Up,
+            "0" => Direction::Right,
+            "1" => Direction::Down,
+            "2" => Direction::Left,
+            "3" => Direction::Up,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl Direction {
+    fn apply(&self, y: i64, x: i64) -> (i64, i64) {
+        match self {
+            Direction::Up => (y - 1, x),
+            Direction::Down => (y + 1, x),
+            Direction::Right => (y, x + 1),
+            Direction::Left => (y, x - 1),
+        }
+    }
+
+    fn apply_s(&self, y: i64, x: i64, steps: i64) -> (i64, i64) {
+        match self {
+            Direction::Up => (y - steps, x),
+            Direction::Down => (y + steps, x),
+            Direction::Right => (y, x + steps),
+            Direction::Left => (y, x - steps),
+        }
+    }
+}
+
+#[test]
+fn test_part1() {
+    let input = r#"R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)"#;
+
+    assert_eq!(part1(input), 62);
+}
+
+#[test]
+fn test_part2() {
+    let input = r#"R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)"#;
+
+    assert_eq!(part2(input), 952408144115);
+}
