3996811b6c6a434fe32a6be88825fc1acff23674
Matteo Fiorina <fiorematteo2002@gmail.com>
2023-12-27 05:18:40 +0100

    day 21 part 2

--- 2023/src/day21.rs
+++ 2023/src/day21.rs
@@ -4,6 +4,7 @@ use std::collections::HashSet;
 fn part1(input: &str) -> usize {
     solve_part1(input, 64)
 }
+
 fn solve_part1(input: &str, steps: usize) -> usize {
     let map = input
         .lines()
@@ -19,8 +20,12 @@ fn solve_part1(input: &str, steps: usize) -> usize {
         }
     }
 
+    count_reachable(&map, pos.0, pos.1, steps)
+}
+
+fn count_reachable(map: &Vec<Vec<char>>, start_y: usize, start_x: usize, steps: usize) -> usize {
     let mut queue = HashSet::new();
-    queue.insert(pos);
+    queue.insert((start_y, start_x));
     for _ in 0..steps {
         let old_queue = queue.clone();
         queue.clear();
@@ -39,6 +44,76 @@ fn solve_part1(input: &str, steps: usize) -> usize {
         }
     }
     queue.len()
+}
+
+#[aoc(day21, part2)]
+fn part2(input: &str) -> i64 {
+    let map = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut start = (0, 0);
+    for (y, line) in map.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if *c == 'S' {
+                start = (y, x);
+            }
+        }
+    }
+
+    let target = 26501365;
+    let (x_1, x_2, x_3) = (64, 195, 326);
+
+    let mut y_1 = 0;
+    let mut y_2 = 0;
+    let mut y_3 = 0;
+
+    let mut queue = HashSet::new();
+    queue.insert((start.0, start.1));
+    for i in 0.. {
+        let old_queue = queue.clone();
+        queue.clear();
+        for (y, x) in old_queue.into_iter() {
+            for (dy, dx) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+                let n_y = y as i64 + dy;
+                let n_x = x as i64 + dx;
+                let mod_y = n_y.rem_euclid(map.len() as i64);
+                let mod_x = n_x.rem_euclid(map[0].len() as i64);
+                if map[mod_y as usize][mod_x as usize] != '#' {
+                    queue.insert((n_y as usize, n_x as usize));
+                }
+            }
+        }
+        if i == x_1 {
+            y_1 = queue.len() as i64;
+        }
+        if i == x_2 {
+            y_2 = queue.len() as i64;
+        }
+        if i == x_3 {
+            y_3 = queue.len() as i64;
+            break;
+        }
+    }
+
+    // lagrange interpolation
+    let f = |x: i64| {
+        let x = x as f64;
+        let (x_1, x_2, x_3) = (x_1 as f64, x_2 as f64, x_3 as f64);
+        let (y_1, y_2, y_3) = (y_1 as f64, y_2 as f64, y_3 as f64);
+
+        let a = y_1 * (x - x_2) * (x - x_3) / ((x_1 - x_2) * (x_1 - x_3));
+        let b = y_2 * (x - x_3) * (x - x_1) / ((x_2 - x_1) * (x_2 - x_3));
+        let c = y_3 * (x - x_1) * (x - x_2) / ((x_3 - x_1) * (x_3 - x_2));
+        a + b + c
+    };
+
+    assert_eq!(f(x_1), y_1 as _);
+    assert_eq!(f(x_2), y_2 as _);
+    assert_eq!(f(x_3), y_3 as _);
+
+    f(target - 1) as _
 }
 
 #[test]
